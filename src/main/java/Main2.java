b8eccf87cfc2eb1276edbcaeb619393010d89cab
毛 <1296193245@qq.com>
2022-01-11 20:25:02 +0800

    imitating_doudizhu_shuffling_and_dealing_cards_games

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -17,6 +17,51 @@ public class Main2
 {
     public static void main(String[] args)
     {
+        //------------------------------------------------------
+        long startTime = System.nanoTime();   //获取开始时间
+        //------------------------------------------------------
+        run();
+        System.out.println();
+        System.out.println();
+        //------------------------------------------------------
+        long endTime = System.nanoTime(); //获取结束时间
+        if ((endTime - startTime) < 1000000)
+        {
+            double final_runtime;
+            final_runtime = (endTime - startTime);
+            final_runtime = final_runtime / 1000;
+            System.out.println("算法运行时间： " + final_runtime + "微秒");
+        }
+        else if ((endTime - startTime) >= 1000000 && (endTime - startTime) < 10000000000L)
+        {
+            double final_runtime;
+            final_runtime = (endTime - startTime) / 1000;
+            final_runtime = final_runtime / 1000;
+            System.out.println("算法运行时间： " + final_runtime + "毫秒");
+        }
+        else
+        {
+            double final_runtime;
+            final_runtime = (endTime - startTime) / 10000;
+            final_runtime = final_runtime / 100000;
+            System.out.println("算法运行时间： " + final_runtime + "秒");
+        }
+        Runtime r = Runtime.getRuntime();
+        float memory;
+        memory = r.totalMemory();
+        memory = memory / 1024 / 1024;
+        System.out.printf("JVM总内存：%.3fMB\n", memory);
+        memory = r.freeMemory();
+        memory = memory / 1024 / 1024;
+        System.out.printf(" 空闲内存：%.3fMB\n", memory);
+        memory = r.totalMemory() - r.freeMemory();
+        memory = memory / 1024 / 1024;
+        System.out.printf("已使用的内存：%.4fMB\n", memory);
+        //------------------------------------------------------
+    }
+
+    public static void run()
+    {
         // 1.准备牌
         String[] arr1 = {"黑桃", "红桃", "方片", "梅花"};
         String[] arr2 = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
